1f6a9d134c7ef241abb1d4c2b7ca09a489c78623
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-06-05 20:43:28 +0300

    Initial commit

---
+++ app/src/main/java/com/naomi/notification/App.java
@@ -0,0 +1,9 @@
+package com.naomi.notification;
+
+import android.app.Application;
+
+public class App extends Application {
+    public static final String CHANNEL_1_ID = "channel1";
+    public static final String CHANNEL_2_ID = "channel2";
+
+}
